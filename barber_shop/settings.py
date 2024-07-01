c86987731621e4bf654bc8196e57c1057994653d
Huss Al Dafai <h_aldafai@hotmail.com>
2024-07-01 13:07:08 +0000

    added base.html with navbar and footer

--- barber_shop/settings.py
+++ barber_shop/settings.py
@@ -25,6 +25,7 @@ SECRET_KEY = 'django-insecure-9@gd5__*%_0s=i2+rh4aeiqegh^n=!99lbwuk&q1i+r4n7yuuy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+
 ALLOWED_HOSTS = [
     'localhost',
     '.herokuapp.com',
@@ -122,6 +123,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
