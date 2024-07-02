5c11a7eb58988ad680ad34afd2ba2ff5c4a31f67
Huss Al Dafai <h_aldafai@hotmail.com>
2024-07-02 15:06:39 +0000

    deploying to heroku

--- barber_shop/settings.py
+++ barber_shop/settings.py
@@ -145,6 +145,10 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+# Media files (Uploaded files)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 
 
 # Default primary key field type
